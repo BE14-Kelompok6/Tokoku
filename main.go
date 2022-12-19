05fcf5f67f3005b08b056a7e09435a609e25c4c9
kgriffinh <griffinh29@gmail.com>
2022-12-19 22:54:09 +0800

    sementara

--- main.go
+++ main.go
@@ -1,5 +1,81 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"tokoku/config"
+)
+
 func main() {
+	var inputMenu int
+	var cfg = config.ReadConfig()
+	var conn = config.ConnectSQL(*cfg)
+	
+
+	for inputMenu != 9 {
+		fmt.Println("WELCOME TO TOKOKU MENU")
+		fmt.Println("1. Login Admin")
+		fmt.Println("2. Login Pegawai")
+		fmt.Println("9. Exit Program")
+		fmt.Print("Masukkan pilihan : ")
+		fmt.Scanln(&inputMenu)
+		if inputMenu == 1 {
+			// buat var ? 
+			fmt.Print("Username : ")
+			fmt.Scanln(&)
+			fmt.Print("Password : ")
+			fmt.Scanln(&)
+
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+
+			if blablabla {
+				isLogin := true
+				for isLogin {
+					fmt.Println("1. Tambah daftar pegawai")
+					fmt.Println("2. Hapus daftar pegawai")
+					fmt.Println("3. Hapus daftar barang")
+					fmt.Println("9. Exit") // atau kembali?
+					fmt.Print("Masukkan pilihan : ")
+					fmt.Scanln(&inputMenu)
+
+					if inputMenu == 1 {
+						// buat var ?
+						fmt.Print("Mendaftarkan username : ")
+						fmt.Scanln(&)
+						fmt.Print("Mendaftarkan password : ")
+						fmt.Scanln(&)
+
+
+						if err != nil {
+							fmt.Println(err.Error())
+						}
+					} else if inputMenu == 2 {
+						// buat var ?
+						fmt.Print("Hapus id/nama(?) pegawai : ")
+						fmt.Scanln(&)
+						
+					} else if inputMenu == 3 {
+						// buat var ?
+						fmt.Print("Daftar barang yang ingin dihapus : ")
+						fmt.Scanln(&)
+					} else if inputMenu == 9 {
+						isLogin = false
+					}
+				}
+
+			} else {
+				fmt.Println("Gagal untuk login")
+			}
+
+		} else if inputMenu == 2 {
+			// buat var ?
+			fmt.Print("Username : ")
+			fmt.Scanln(&)
+			fmt.Print("Password : ")
+			fmt.Scanln(&)
+		}
+	}
 
 }
--- user/user.go
+++ user/user.go
@@ -1,1 +1,86 @@
 package user
+
+import ""
+
+type User struct {
+	ID int
+	Nama string
+	Password string
+	Role int
+}
+
+type AuthMenu struct {
+	DB *sql.DB
+}
+
+func (am *AuthMenu) Duplicate(name string) bool {
+	res := am.DB.QueryRow("SELECT id FROM users where nama = ?", name)
+	var idExist int
+	err := res.Scan(&idExist)
+	if err != nil {
+		log.Println("Result scan error", err.Error())
+		return false
+	}
+	if idExist > 0 {
+		return true
+	}
+	return false
+}
+
+func (am *AuthMenu) Register(newUser User) (bool, error) {
+	registerQry, err := am.DB.Prepare("INSERT INTO users (nama, password) values (?,?)")
+	if err != nil {
+		log.Println("prepare insert user ", err.Error())
+		return false, errors.New("prepare statement insert user error")
+	}
+
+	if am.Duplicate(newUser.Nama) {
+		log.Println("duplicated information")
+		return false, errors.New("nama sudah digunakan")
+	}
+
+	res, err := registerQry.Exec(newUser.Nama, newUser.Password)
+	if err != nil {
+		log.Println("insert user ", err.Error())
+		return false, errors.New("insert user error")
+	}
+
+	affRows, err := res.RowsAffected()
+	if err != nil {
+		log.Println("after insert user ", err.Error())
+		return false, errors.New("error setelah insert")
+	}
+
+	if affRows <= 0 {
+		log.Println("no record affected")
+		return true, errors.New("no record")
+	}
+
+	return true, nil
+}
+
+func (am *AuthMenu) LoginPegawai(nama string, password string) (User, error) {
+	loginQry, err := am.DB.Prepare("SELECT id FROM users WHERE nama = ? AND password = ?")
+	if err != nil {
+		log.Println("prepare insert user ", err.Error())
+		return User{}, errors.New("prepare statement insert user error")
+	}
+
+	row := loginQry.QueryRow(nama, password)
+
+	if row.Err() != nil {
+		log.Println("login query ", row.Err().Error())
+		return User{}, errors.New("tidak bisa login, data tidak ditemukan")
+	}
+	res := User{}
+	err = row.Scan(&res.ID)
+
+	if err != nil {
+		log.Println("after login query ", err.Error())
+		return User{}, errors.New("tidak bisa login, kesalahan setelah error")
+	}
+
+	res.Nama = nama
+
+	return res, nil
+}
