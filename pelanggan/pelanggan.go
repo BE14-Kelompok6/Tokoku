f2e2d5d175896fd66b93bb30bef1de673fb608e7
root <root@DESKTOP-00KIOGB.localdomain>
2022-12-21 14:41:51 +0700

    add hapusPelanggan method

--- pelanggan/pelanggan.go
+++ pelanggan/pelanggan.go
@@ -68,3 +68,31 @@ func (pm *PelangganMenu) TampilkanPelanggan() {
 	}
 
 }
+
+func (pm *PelangganMenu) HapusPelanggan(id int) (bool, error) {
+	hapuspelangganQry, err := pm.DB.Prepare("DELETE FROM pelanggan where id = ?")
+	if err != nil {
+		log.Println("prepare hapus pelanggan ", err.Error())
+		return false, errors.New("prepare statement hapus pelanggan error")
+	}
+
+	res, err := hapuspelangganQry.Exec(id)
+	if err != nil {
+		log.Println("hapus pelanggan", err.Error())
+		return false, errors.New("hapus pelanggan error")
+	}
+
+	affRows, err := res.RowsAffected()
+
+	if err != nil {
+		log.Println("after hapus pelanggan ", err.Error())
+		return false, errors.New("error setelah hapus pelanggan")
+	}
+
+	if affRows <= 0 {
+		log.Println("no record affected")
+		return false, errors.New("no record")
+	}
+
+	return true, nil
+}
