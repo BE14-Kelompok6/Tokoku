32fe30039e95de94c6e26a107f9e99fd543a1050
kgriffinh <griffinh29@gmail.com>
2022-12-21 12:05:34 +0800

    update barang

--- barang/barang.go
+++ barang/barang.go
@@ -3,6 +3,7 @@ package barang
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 	"log"
 )
 
@@ -47,4 +48,58 @@ func (bm *BarangMenu) TambahBarang(newBarang Barang) (int, error) {
 	id, _ := res.LastInsertId()
 
 	return int(id), nil
+}
+
+func (bm *BarangMenu) UpdateBarang(newNamaBarang string, newStok int, id int) (bool, error) {
+	updateBarangQry, err := bm.DB.Prepare("UPDATE barang SET nama_barang = ?, stok = ? WHERE id = ?")
+	if err != nil {
+		log.Println("prepare update barang ", err.Error())
+		return false, errors.New("prepare statement update barang error")
+	}
+
+	res, err := updateBarangQry.Exec(newNamaBarang, newStok)
+	if err != nil {
+		log.Println("update barang", err.Error())
+		return false, errors.New("update barang error")
+	}
+
+	affRows, err := res.RowsAffected()
+
+	if err != nil {
+		log.Println("after update barang ", err.Error())
+		return false, errors.New("error setelah update barang")
+	}
+
+	if affRows <= 0 {
+		log.Println("no record affected")
+		return false, errors.New("no record")
+	}
+
+	return true, nil
+}
+
+func (bm *BarangMenu) TampilkanBarang() {
+	rows, err := bm.DB.Query("SELECT id, nama_barang, stok FROM barang")
+	if err != nil {
+		log.Println("tampilkan barang ", err.Error())
+		fmt.Println(errors.New("tampilkan barang error"))
+	}
+
+	// declare empty post variable
+	type barang struct {
+		id   int
+		Nama string
+		Stok int
+	}
+
+	// iterate over rows
+	for rows.Next() {
+		err = rows.Scan(&barang.id, &nama, &stok)
+		if err != nil {
+			log.Println("tampilkan barang ", err.Error())
+			fmt.Println(errors.New("tampilkan barang error"))
+		}
+		fmt.Println(barang)
+	}
+
 }
