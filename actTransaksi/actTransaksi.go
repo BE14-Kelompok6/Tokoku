9519d4c4b1f3aae19f57e022f4035d6daed7f26f
root <root@DESKTOP-00KIOGB.localdomain>
2022-12-22 13:59:25 +0700

    Edit error log

--- actTransaksi/actTransaksi.go
+++ actTransaksi/actTransaksi.go
@@ -19,22 +19,22 @@ type ActTransaksiMenu struct {
 func (atm *ActTransaksiMenu) TambahActTransaksi(newActTransaksi ActTransaksi) (bool, error) {
 	addTransaksiQry, err := atm.DB.Prepare("INSERT INTO aktivitas_transaksi (transaksi_id,barang_id, qty) values (?,?,?)")
 	if err != nil {
-		log.Println("prepare insert pelanggan ", err.Error())
-		return false, errors.New("prepare statement insert pelanggan error")
+		log.Println("prepare add actTransaksi ", err.Error())
+		return false, errors.New("prepare actTransaksi error")
 	}
 
 	res, err := addTransaksiQry.Exec(newActTransaksi.Transaksi_id, newActTransaksi.Barang_id, newActTransaksi.Qty)
 
 	if err != nil {
 		log.Println("insert transaksi ", err.Error())
-		return false, errors.New("insert transaksi error")
+		return false, errors.New("insert tactTransaksi error")
 	}
 
 	affRows, err := res.RowsAffected()
 
 	if err != nil {
-		log.Println("after insert transaksi ", err.Error())
-		return false, errors.New("error setelah insert transaksi")
+		log.Println("after insert actTransaksi ", err.Error())
+		return false, errors.New("error setelah insert tactTransaksi")
 	}
 
 	if affRows <= 0 {
